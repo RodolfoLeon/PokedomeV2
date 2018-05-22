8b47a0c500062e8fdd0e819c3b1402e94a850e98
Avalos Leon <rodoleon@amazon.com>
2018-05-22 13:45:14 -0600

    First commit to initialize repo

---
+++ pokedomev2/src/main/java/com/lab206/pokedomev2/Pokedomev2Application.java
@@ -0,0 +1,12 @@
+package com.lab206.pokedomev2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pokedomev2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pokedomev2Application.class, args);
+	}
+}
