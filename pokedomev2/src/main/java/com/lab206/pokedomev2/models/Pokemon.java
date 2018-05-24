9cbf5afca65740b7f6a59d8dab2d7ef28948eedf
Esteban SP <esteban124@gmail.com>
2018-05-24 12:37:25 -0600

    pokemon model

---
+++ pokedomev2/src/main/java/com/lab206/pokedomev2/models/Pokemon.java
@@ -0,0 +1,46 @@
+package com.lab206.pokedomev2.models;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
+@Table(name="pokemons")
+public class Pokemon {
+	
+	@Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+		
+	private int pk_id;
+ 
+	private String name;
+		
+	private Double pk_height;
+			
+    @Column(updatable=false)
+    @DateTimeFormat(pattern = "MM/dd/yyyy HH:mm:ss")
+    private Date createdAt;
+    @DateTimeFormat(pattern = "MM/dd/yyyy HH:mm:ss")
+    private Date updatedAt;
+    
+    @PrePersist
+    protected void onCreate(){
+        this.createdAt = new Date();
+    }
+    @PreUpdate
+    protected void onUpdate(){
+        this.updatedAt = new Date();
+    }    
+	
+    
+}
